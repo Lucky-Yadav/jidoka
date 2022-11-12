fbb18c543ecb1933710b9cabfbf5ad03c6e86ab7
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-11-13 02:09:22 +0530

    update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,43 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import socketIOClient from 'socket.io-client'
+import "./App.css";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import socketIOClient from "socket.io-client";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Private from "./components/Private";
+import Signup from "./components/Signup";
 
 const App = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    const socket = socketIOClient("http://127.0.0.1:3070/")  
+    const socket = socketIOClient("http://127.0.0.1:3070/");
     socket.on("message", (data) => {
-      setData(data)
-    })
-  
-  }, [])
-  
+      setData(data);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>{ data}</h1>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Private>
+              {" "}
+              <Home />
+            </Private>
+          }
+        ></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/Signup" element={<Signup />}></Route>
+      </Routes>
+      <h1>{data}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
